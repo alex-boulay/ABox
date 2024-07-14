b664b7f5c68ee776e4ef37e86a83489805bd8b96
Alex Boulay <alexandre.boulay59@gmail.com>
2024-07-15 01:59:03 +0200

    Swapchain and ImageViews done. At page 87 Graphics Pipeline basics

--- VkTstApp.cpp
+++ VkTstApp.cpp
@@ -175,6 +175,7 @@ void VkTstApp::initVulkan(){
   pickPhysicalDevice();
   createLogicalDevice();
   createSwapChain();
+  createImageViews();
 }
 
 void VkTstApp::createLogicalDevice(){
@@ -344,6 +345,77 @@ void VkTstApp::createSwapChain(){
   VkSurfaceFormatKHR surfaceFormat = chooseSwapSurfaceFormat(swapChainSupport.formats);
   VkPresentModeKHR presentMode = chooseSwapPresentMode(swapChainSupport.presentModes);
   VkExtent2D extent = chooseSwapExtent(swapChainSupport.capabilities);
+
+  QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
+
+  std::vector<uint32_t> queueFamilyIndices = {
+    indices.graphicsFamily.value(),
+    indices.presentFamily.value()
+  };
+
+  #define SCSC_Mi swapChainSupport.capabilities.minImageCount + 1
+  #define SCSC_Ma swapChainSupport.capabilities.maxImageCount
+  #define SCSC_MinC std::min(SCSC_Ma,SCSC_Mi) + !(SCSC_Ma) * (SCSC_Mi)
+  #define SCSC_CONCURENT (indices.graphicsFamily != indices.presentFamily)
+
+  VkSwapchainCreateInfoKHR createInfo{
+    .sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR,
+    .surface = surface,
+    .minImageCount = SCSC_MinC,
+    .imageFormat = surfaceFormat.format,
+    .imageColorSpace = surfaceFormat.colorSpace,
+    .imageExtent = extent,
+    .imageArrayLayers = 1,
+    .imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT,
+    .imageSharingMode = SCSC_CONCURENT ? VK_SHARING_MODE_CONCURRENT : VK_SHARING_MODE_EXCLUSIVE,
+    .queueFamilyIndexCount = SCSC_CONCURENT * static_cast<uint32_t>(queueFamilyIndices.size()),
+    .pQueueFamilyIndices = SCSC_CONCURENT ? queueFamilyIndices.data() : nullptr,
+    .preTransform = swapChainSupport.capabilities.currentTransform,
+    .compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR,
+    .presentMode = presentMode,
+    .clipped = VK_TRUE,
+    .oldSwapchain = VK_NULL_HANDLE
+  };
+
+  #undef SCSC_CONCURENT
+  #undef SCSC_Ma
+  #undef SCSC_Mi
+  #undef SCSC_MinC
+
+  if(vkCreateSwapchainKHR(device, &createInfo, nullptr, &swapChain) != VK_SUCCESS)
+    throw std::runtime_error("failed to create swap chain !");
+  else
+    std::cout << "Swapchain created !!" << std::endl;
+
+  vkGetSwapchainImagesKHR(device, swapChain, &createInfo.minImageCount,nullptr);
+  swapChainImages.resize(createInfo.minImageCount);
+  vkGetSwapchainImagesKHR(device, swapChain,&createInfo.minImageCount, swapChainImages.data());
+
+  swapChainImageFormat = surfaceFormat.format;
+  swapChainExtent = extent;
+}
+
+void VkTstApp::createImageViews(){
+  swapChainImageViews.resize(swapChainImages.size());
+  #define LOC_SID VK_COMPONENT_SWIZZLE_IDENTITY
+  for (size_t i = 0; i < swapChainImages.size(); i++){
+    VkImageViewCreateInfo createInfo = {
+      .sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO,
+      .image = swapChainImages[i],
+      .viewType = VK_IMAGE_VIEW_TYPE_2D,
+      .format = swapChainImageFormat,
+      .components= { LOC_SID,LOC_SID,LOC_SID,LOC_SID },
+      .subresourceRange = {
+        .aspectMask = VK_IMAGE_ASPECT_COLOR_BIT,
+        .levelCount = 1,
+        .layerCount = 1
+      }
+    };
+    if(vkCreateImageView(device, &createInfo,nullptr, &swapChainImageViews[i]) != VK_SUCCESS)
+      throw std::runtime_error("failed to create image views!");
+  }
+  #undef LOC_SID
+
 }
 
 void VkTstApp::mainLoop(){
@@ -354,6 +426,10 @@ void VkTstApp::mainLoop(){
 }
 
 void VkTstApp::cleanup(){
+  for (VkImageView imageView : swapChainImageViews)
+    vkDestroyImageView(device,imageView, nullptr);
+
+  vkDestroySwapchainKHR(device, swapChain, nullptr);
   vkDestroyDevice(device, nullptr);
   if (enableValidationLayers) DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
   std::cout << "Destroy Debug Util" << std::endl;

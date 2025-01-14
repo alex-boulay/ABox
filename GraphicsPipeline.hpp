8b089779c8aebb82948ae9331033776969c56fea
Alex Boulay <alexandre.boulay59@gmail.com>
2025-01-14 21:41:12 +0100

    Graphics pipeline code suite
    Made most of the main graphics pipeline structures.
    Added a destruct as specific to clear pipeline layout from Vulkan

--- GraphicsPipeline.hpp
+++ GraphicsPipeline.hpp
@@ -3,6 +3,8 @@
 
 #include "SwapchainManager.hpp"
 #include <array>
+#include <iostream>
+#include <stdexcept>
 #include <vulkan/vulkan.h>
 #include <vulkan/vulkan_core.h>
 
@@ -12,13 +14,17 @@ static const std::array<VkDynamicState, 2> dynamicStates = {
 };
 
 class GraphicsPipeline {
-  VkViewport viewport;
-  VkRect2D   scissor;
+  VkViewport       viewport;
+  VkRect2D         scissor;
+  VkPipelineLayout pipelineLayout;
+  const VkDevice  &device;
   // bindShaderHandlers here
   //
   GraphicsPipeline(
-      SwapchainManager sm
+      const SwapchainManager &sm,
+      const VkDevice         &device
   )
+      : device(device)
   {
     VkPipelineDynamicStateCreateInfo dynamicState{
         .sType = VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO,
@@ -65,6 +71,81 @@ class GraphicsPipeline {
         .scissorCount  = 1u,
         .pScissors     = &scissor
     };
+    VkPipelineRasterizationStateCreateInfo rasterizer{
+        .sType =
+            VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_PROVOKING_VERTEX_STATE_CREATE_INFO_EXT,
+        .pNext                   = nullptr,
+        .flags                   = 0u,
+        .depthClampEnable        = VK_FALSE,
+        .rasterizerDiscardEnable = VK_FALSE,
+        .polygonMode             = VK_POLYGON_MODE_FILL,
+        .cullMode                = 0u,
+        .frontFace               = VK_FRONT_FACE_COUNTER_CLOCKWISE,
+        .depthBiasEnable         = VK_FALSE,
+        .depthBiasConstantFactor = 0.0f,
+        .depthBiasClamp          = 0.0f,
+        .depthBiasSlopeFactor    = 0.0f,
+        .lineWidth               = 1.0f
+    };
+    VkPipelineMultisampleStateCreateInfo multisampling{
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO,
+        .pNext = nullptr,
+        .flags = 0u,
+        .rasterizationSamples  = VK_SAMPLE_COUNT_1_BIT,
+        .sampleShadingEnable   = VK_FALSE,
+        .minSampleShading      = 1.0f,
+        .pSampleMask           = nullptr,
+        .alphaToCoverageEnable = VK_FALSE,
+        .alphaToOneEnable      = VK_FALSE
+    };
+    VkPipelineColorBlendAttachmentState colorBlendAttachment{
+        .colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT |
+                          VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT,
+        .blendEnable         = VK_FALSE,
+        .srcColorBlendFactor = VK_BLEND_FACTOR_ONE,
+        .dstColorBlendFactor = VK_BLEND_FACTOR_ZERO,
+        .colorBlendOp        = VK_BLEND_OP_ADD,
+        .srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE,
+        .dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO,
+        .alphaBlendOp        = VK_BLEND_OP_ADD
+    };
+
+    VkPipelineColorBlendStateCreateInfo colorBlending{
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO,
+        .pNext = nullptr,
+        .flags = 0u,
+        .logicOpEnable   = VK_FALSE,
+        .logicOp         = VK_LOGIC_OP_COPY,
+        .attachmentCount = 1u,
+        .pAttachments    = &colorBlendAttachment,
+        .blendConstants  = {0.0f, 0.0f, 0.0f, 0.0f}
+    };
+
+    VkPipelineLayoutCreateInfo pipelineLayoutInfo{
+        .sType                  = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO,
+        .pNext                  = nullptr,
+        .flags                  = 0u,
+        .setLayoutCount         = 0u,
+        .pSetLayouts            = nullptr,
+        .pushConstantRangeCount = 0u,
+        .pPushConstantRanges    = nullptr
+    };
+    VkResult res = VK_SUCCESS;
+    res          = vkCreatePipelineLayout(
+        device, // TODO: add Device Management to Pipeline
+        &pipelineLayoutInfo,
+        nullptr,
+        &pipelineLayout
+    );
+    if (res != VK_SUCCESS) {
+      std::cout << "Vulkan Error value : " << res << std::endl;
+      throw std::runtime_error("failed to create pipeline layout !");
+    }
+  }
+  void CreateRenderPass() { VkAttachmentDescription colorAttachment{}; }
+  ~GraphicsPipeline()
+  {
+    vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
   }
 };
 

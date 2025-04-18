7617f5a87bc6a6b5c03c315133d8c97f6e99f816
Alexandre Boulay <alexandre.boulay59@gmail.com>
2025-04-18 17:33:32 +0200

    ShaderHandler Modification on Modules Management
    Now it'll be the DeviceHandler Responsability to Create and Handle
    ShaderModules and PipelineShaderStages, mapping might need to happend as
    both structures(Module and PipelineStage shall be bound using parameters
    inside the ShaderHandler)

--- ShaderHandler.hpp
+++ ShaderHandler.hpp
@@ -12,112 +12,94 @@
 #include <optional>
 #include <string>
 #include <tuple>
-#include <unordered_map>
-#include <utility>
 #include <vector>
 #include <vulkan/vulkan_core.h>
 
 #define OPENGL_CHOSEN_VERSION 450
-#define VULKAN_CHOSEN_VERSION                                                  \
-  glslang::EShTargetVulkan_1_3 // need to bind it to current API number
-#define SPIRV_CHOSEN_VERSION glslang::EShTargetSpv_1_5
+#define VULKAN_CHOSEN_VERSION glslang::EShTargetVulkan_1_3    // need to bind it to current API number
+#define SPIRV_CHOSEN_VERSION  glslang::EShTargetSpv_1_5
 
-typedef std::tuple<std::string, EShLanguage, VkShaderStageFlagBits>
-    stageExtention;
+typedef std::tuple<std::string, EShLanguage, VkShaderStageFlagBits> stageExtention;
 
-class StageExtentionHandler {
+class StageExtentionHandler
+{
+    StageExtentionHandler() = default;
+    /** @brief map matching an extension to a shader bit
+     * same shader can be used in different stages the infomation
+     * provided is mostly to categorise them then endvalue of the
+     * shader flag mask
+     */
+    inline static const std::array<stageExtention, 14> map{
+        {{".vert", EShLangVertex, VK_SHADER_STAGE_VERTEX_BIT},
+         {".frag", EShLangFragment, VK_SHADER_STAGE_FRAGMENT_BIT},
+         {".comp", EShLangCompute, VK_SHADER_STAGE_COMPUTE_BIT},
+         {".geom", EShLangGeometry, VK_SHADER_STAGE_GEOMETRY_BIT},
+         {".tesc", EShLangTessControl, VK_SHADER_STAGE_TESSELLATION_CONTROL_BIT},
+         {".tese", EShLangTessEvaluation, VK_SHADER_STAGE_TESSELLATION_EVALUATION_BIT},
+         {".rgen", EShLangRayGen, VK_SHADER_STAGE_RAYGEN_BIT_KHR},
+         {".rahit", EShLangAnyHit, VK_SHADER_STAGE_ANY_HIT_BIT_KHR},
+         {".rchit", EShLangClosestHit, VK_SHADER_STAGE_CLOSEST_HIT_BIT_KHR},
+         {".rmiss", EShLangMiss, VK_SHADER_STAGE_MISS_BIT_KHR},
+         {".rint", EShLangIntersect, VK_SHADER_STAGE_INTERSECTION_BIT_KHR},
+         {".rcall", EShLangCallable, VK_SHADER_STAGE_CALLABLE_BIT_KHR},
+         {".task", EShLangTask, VK_SHADER_STAGE_TASK_BIT_EXT},
+         {".mesh", EShLangMesh, VK_SHADER_STAGE_MESH_BIT_EXT}}
+    };
 
-  StageExtentionHandler() = default;
-  /** @brief map matching an extension to a shader bit
-   * same shader can be used in different stages the infomation
-   * provided is mostly to categorise them then endvalue of the
-   * shader flag mask
-   */
-  inline static const std::array<stageExtention, 14> map{
-      {{".vert", EShLangVertex, VK_SHADER_STAGE_VERTEX_BIT},
-       {".frag", EShLangFragment, VK_SHADER_STAGE_FRAGMENT_BIT},
-       {".comp", EShLangCompute, VK_SHADER_STAGE_COMPUTE_BIT},
-       {".geom", EShLangGeometry, VK_SHADER_STAGE_GEOMETRY_BIT},
-       {".tesc", EShLangTessControl, VK_SHADER_STAGE_TESSELLATION_CONTROL_BIT},
-       {".tese",
-        EShLangTessEvaluation,
-        VK_SHADER_STAGE_TESSELLATION_EVALUATION_BIT},
-       {".rgen", EShLangRayGen, VK_SHADER_STAGE_RAYGEN_BIT_KHR},
-       {".rahit", EShLangAnyHit, VK_SHADER_STAGE_ANY_HIT_BIT_KHR},
-       {".rchit", EShLangClosestHit, VK_SHADER_STAGE_CLOSEST_HIT_BIT_KHR},
-       {".rmiss", EShLangMiss, VK_SHADER_STAGE_MISS_BIT_KHR},
-       {".rint", EShLangIntersect, VK_SHADER_STAGE_INTERSECTION_BIT_KHR},
-       {".rcall", EShLangCallable, VK_SHADER_STAGE_CALLABLE_BIT_KHR},
-       {".task", EShLangTask, VK_SHADER_STAGE_TASK_BIT_EXT},
-       {".mesh", EShLangMesh, VK_SHADER_STAGE_MESH_BIT_EXT}}
-  };
+public:
+    template<typename T> inline static constexpr auto at(T key)
+    {
+        return std::find_if(map.begin(), map.end(), [key](const auto &v) { return key == std::get<T>(v); });
+    }
 
-   public:
-  template <typename T>
-  inline static constexpr auto at(
-      T key
-  )
-  {
-    return std::find_if(map.begin(), map.end(), [key](const auto &v) {
-      return key == std::get<T>(v);
-    });
-  }
+    template<typename T> inline static constexpr bool contains(T key) { return map.end() != at(key); }
 
-  template <typename T>
-  inline static constexpr bool contains(
-      T key
-  )
-  {
-    return map.end() != at(key);
-  }
-
-  [[nodiscard]] inline std::optional<EShLanguage> getStageExt(
-      std::string stageExt
-  ) noexcept
-  {
-    return StageExtentionHandler::contains(stageExt)
-               ? std::optional<EShLanguage>{std::get<EShLanguage>(
-                     *(StageExtentionHandler::at(stageExt))
-                 )}
-               : std::nullopt;
-  }
+    [[nodiscard]] inline std::optional<EShLanguage>   getStageExt(std::string stageExt) noexcept
+    {
+        return StageExtentionHandler::contains(stageExt) ?
+                   std::optional<EShLanguage>{std::get<EShLanguage>(*(StageExtentionHandler::at(stageExt)))} :
+                   std::nullopt;
+    }
 };
 
-enum class SourcePlatform {
-  GLSL,   // GLSL (OpenGL Shading Language)
-  HLSL,   // HLSL (High-Level Shading Language, DirectX)
-  OpenCL, // OpenCL C
-  CUDA,   // CUDA (NVIDIA)
-  WGSL,   // WGSL (WebGPU Shading Language)
-  Rust,   // Rust (using Vulkan bindings)
-  Python, // Python (using PyOpenCL or PyVulkan)
-  Unknown // platform not specified or unknown
+enum class SourcePlatform
+{
+    GLSL,      // GLSL (OpenGL Shading Language)
+    HLSL,      // HLSL (High-Level Shading Language, DirectX)
+    OpenCL,    // OpenCL C
+    CUDA,      // CUDA (NVIDIA)
+    WGSL,      // WGSL (WebGPU Shading Language)
+    Rust,      // Rust (using Vulkan bindings)
+    Python,    // Python (using PyOpenCL or PyVulkan)
+    Unknown    // platform not specified or unknown
 };
 
 // extern const std::map<std::string, SourcePlatform> extensionToPlatform;
 
-[[nodiscard]] inline SourcePlatform getPlatformExt(std::string platExt);
+[[nodiscard]] inline SourcePlatform     getPlatformExt(std::string platExt);
 
-[[nodiscard]] inline glslang::EShSource
-    getEShSource(SourcePlatform sourcePlatform);
+[[nodiscard]] inline glslang::EShSource getEShSource(SourcePlatform sourcePlatform);
 
 /** @brief enum to represent a result status type for files */
-typedef enum VkFileResult {
-  VK_FILE_SUCCESS         = 0,
-  VK_FILE_EXTENTION_ERROR = 1,
-  VK_FILE_NO_MATCH_ERROR  = 2,
-  VK_FILE_UNKNOWN_ERROR   = 3,
-  VK_FILE_EMPTY_FOLDER    = 4,
-  VK_NO_FILE_FOUND        = 5,
-  VK_FILE_NOT_A_SHADER    = 6
+typedef enum VkFileResult
+{
+    VK_FILE_SUCCESS         = 0,
+    VK_FILE_EXTENTION_ERROR = 1,
+    VK_FILE_NO_MATCH_ERROR  = 2,
+    VK_FILE_UNKNOWN_ERROR   = 3,
+    VK_FILE_EMPTY_FOLDER    = 4,
+    VK_NO_FILE_FOUND        = 5,
+    VK_FILE_NOT_A_SHADER    = 6
 } VkFileResult;
 
 // Maybe not the best implementation ? modules and devices shall be set
 // elsewhere ?
-struct shaderBind {
-  const VkDevice *device;
-  VkShaderModule  module;
+struct shaderBind
+{
+    const VkDevice *device;
+    VkShaderModule  module;
 };
+
 /**class ShaderBindMap {
   std::vector<shaderBind>      sBinds; // map a Shader to a Device
   [[nodiscard]] bool           contains(const VkDevice &device) const noexcept;
@@ -129,92 +111,46 @@ struct shaderBind {
  * @member data contains the file data as a vector of chars
  * @member mask
  */
-class ShaderDataFile {
-  const std::string           name;     // name without extensions
-  const std::vector<uint32_t> code;     // Spirv output
-  const stageExtention *const stage;    // Pipeline stage for the shader
-  const SourcePlatform        platform; // in case of recompilation ?
-
-  std::unordered_map<VkDevice, VkShaderModule> sBinds;
-
-   public:
-  ShaderDataFile(
-      const std::string           &name_,
-      const std::vector<uint32_t> &code_,
-      const stageExtention *const  stage_,
-      const SourcePlatform        &platform_
-  )
-      : name(name_)
-      , code(code_)
-      , stage(stage_)
-      , platform(platform_)
-  {
-  }
-
-  /**
-   * @brief load a ShaderModule into the target device
-   * @param the targetted device
-   * @return true if it allocated one
-   */
-  [[nodiscard]] VkResult load(const VkDevice &device_);
-
-  /** @brief unload Shader Module it has an instance
-   * loaded on the given device and free module memory
-   * @return true if it destroyed false otherwise
-   * */
-  [[nodiscard]] VkResult unload(const VkDevice &device);
-
-  ~ShaderDataFile()
-  {
-    for (auto bind : sBinds) {
-      (void)unload(bind.first);
+class ShaderDataFile
+{
+    const std::string           name;        // name without extensions
+    const std::vector<uint32_t> code;        // Spirv output
+    const stageExtention *const stage;       // Pipeline stage for the shader
+    const SourcePlatform        platform;    // in case of recompilation ?
+                                             // ShaderDataFile doesn't handle allocations
+public:
+    ShaderDataFile(const std::string           &name_,
+                   const std::vector<uint32_t> &code_,
+                   const stageExtention *const  stage_,
+                   const SourcePlatform        &platform_)
+        : name(name_)
+        , code(code_)
+        , stage(stage_)
+        , platform(platform_)
+    {
     }
-  }
 
-  ShaderDataFile(const ShaderDataFile &other) = delete; // No Copy -> Unique
-  ShaderDataFile &operator=(const ShaderDataFile &other
-  )                                           = delete; // No Copy -> Unique
-  ShaderDataFile(
-      ShaderDataFile &&other
-  ) noexcept
-      : name(other.name)
-      , code(other.code)
-      , stage(other.stage)
-      , platform(other.platform)
-  {
-    sBinds = std::exchange(other.sBinds, {});
-  }
+    ~ShaderDataFile() = default;
 
-  ShaderDataFile &operator=(
-      ShaderDataFile &&other
-  ) noexcept
-  {
-    ShaderDataFile temp(std::move(other));
-    std::swap(sBinds, temp.sBinds);
-    return *this;
-  }
+    ShaderDataFile(const ShaderDataFile &other)                     = default;
+    ShaderDataFile &operator=(const ShaderDataFile &other)          = delete;    // stage cannot be modified
+    ShaderDataFile(ShaderDataFile &&other) noexcept                 = default;
+    ShaderDataFile      &operator=(ShaderDataFile &&other) noexcept = delete;    // stage connot be modified
 
-  [[nodiscard]] inline operator VkShaderModuleCreateInfo() const;
+    [[nodiscard]] inline operator VkShaderModuleCreateInfo() const;
 
-  std::string getName() const { return name; }
+    std::string          getName() const { return name; }
 
-  inline std::optional<VkPipelineShaderStageCreateInfo> getPplStageCI(
-      VkDevice device
-  ) const
-  {
-    return (sBinds.contains(device) && device != VK_NULL_HANDLE)
-               ? std::make_optional(VkPipelineShaderStageCreateInfo{
-                     .sType =
-                         VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
-                     .pNext  = nullptr,
-                     .flags  = 0u,
-                     .stage  = std::get<VkShaderStageFlagBits>(*stage),
-                     .module = sBinds.at(device),
-                     .pName  = name.c_str(),
-                     .pSpecializationInfo = nullptr
-                 })
-               : std::nullopt;
-  }
+    [[nodiscard]] inline VkPipelineShaderStageCreateInfo getPSSCI(VkShaderModule shm) const
+    {
+        return {.sType               = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
+                .pNext               = nullptr,
+                .flags               = 0u,
+                .stage               = std::get<VkShaderStageFlagBits>(*stage),
+                .module              = shm,
+                .pName               = name.c_str(),
+                .pSpecializationInfo = nullptr};
+    }
 };
 
 /**
@@ -222,110 +158,90 @@ class ShaderDataFile {
  * path
  * @member sDatas vector of Shaders datas
  */
-class ShaderHandler {
-  bool                        isGlsInit = false;
-  std::vector<ShaderDataFile> sDatas;
-  // Devices are used to bind shader to a device when loading so unloading can
-  // be done automaticly;
-  //
-  // Should be handled by the DeviceHandler to have a binding to a shader
-  std::vector<VkDevice> devices; // Usualy one, only exist if shaders are loaded
+class ShaderHandler
+{
+    bool                        isGlsInit = false;
+    std::vector<ShaderDataFile> sDatas;
+    // Devices are used to bind shader to a device when loading so unloading can
+    // be done automaticly;
+    //
+    // Should be handled by the DeviceHandler to have a binding to a shader
+    std::vector<VkDevice>       devices;    // Usualy one, only exist if shaders are loaded
 
-  /**
-   * @brief function to initialize gls
-   * @return true if GlsInit is already init or init succeded else false
-   */
-  inline bool initGlsLang()
-  {
-    return isGlsInit ? true : (isGlsInit = glslang::InitializeProcess());
-  }
+    /**
+     * @brief function to initialize gls
+     * @return true if GlsInit is already init or init succeded else false
+     */
+    inline bool                 initGlsLang() { return isGlsInit ? true : (isGlsInit = glslang::InitializeProcess()); }
 
-  /**@brief uninitialise glsl*/
-  inline void finalizeGlsLang()
-  {
-    if (isGlsInit && !(isGlsInit ^= isGlsInit)) {
-      glslang::FinalizeProcess();
+    /**@brief uninitialise glsl*/
+    inline void                 finalizeGlsLang()
+    {
+        if (isGlsInit && !(isGlsInit ^= isGlsInit))
+        {
+            glslang::FinalizeProcess();
+        }
     }
-  }
 
-   public:
-  ShaderHandler() { ShaderHandler("../shaders/"); }
+public:
+    ShaderHandler() { ShaderHandler("../shaders/"); }
 
-  ShaderHandler(
-      std::filesystem::path folder
-  )
-  {
-    ShaderHandler({folder});
-  }
+    ShaderHandler(std::filesystem::path folder) { ShaderHandler({folder}); }
 
-  ShaderHandler(
-      std::initializer_list<std::filesystem::path> folderNames
-  )
-  {
-    initGlsLang();
-    for (const std::filesystem::path &folder : folderNames) {
-      loadShaderDataFromFolder(folder);
+    ShaderHandler(std::initializer_list<std::filesystem::path> folderNames)
+    {
+        initGlsLang();
+        for (const std::filesystem::path &folder : folderNames)
+        {
+            loadShaderDataFromFolder(folder);
+        }
     }
-  }
 
-  ~ShaderHandler() { finalizeGlsLang(); }
+    ~ShaderHandler() { finalizeGlsLang(); }
 
-  ShaderHandler(const ShaderHandler &other
-  )          = delete; // no Copy -> ShaderDataFile are unique
-  ShaderHandler &operator=(const ShaderHandler &other
-  ) noexcept = delete;                           // no Copy
-  ShaderHandler(ShaderHandler &&other) = delete; // not movable atm
-  ShaderHandler &operator=(ShaderHandler &&other
-  ) noexcept                           = delete; // not movable atm
+    ShaderHandler(const ShaderHandler &other)                     = delete;    // no Copy -> ShaderDataFile are unique
+    ShaderHandler &operator=(const ShaderHandler &other) noexcept = delete;    // no Copy
+    ShaderHandler(ShaderHandler &&other)                          = delete;    // not movable atm
+    ShaderHandler              &operator=(ShaderHandler &&other) noexcept = delete;    // not movable atm
 
-  /**
-   * @brief load data from given shader folder with expected extensions
-   * @param filesystem::path the directory to load from
-   * @return uint16_t the number of loaded files into the vector
-   */
-  uint32_t loadShaderDataFromFolder(const std::filesystem::path &dirPath);
+    /**
+     * @brief load data from given shader folder with expected extensions
+     * @param filesystem::path the directory to load from
+     * @return uint16_t the number of loaded files into the vector
+     */
+    uint32_t                    loadShaderDataFromFolder(const std::filesystem::path &dirPath);
 
-  /**
-   * @brief
-   * @return std::string
-   */
-  const std::string loadShaderFromFile(const std::filesystem::path &shaderFile);
+    /**
+     * @brief
+     * @return std::string
+     */
+    const std::string           loadShaderFromFile(const std::filesystem::path &shaderFile);
 
-  /**
-   * @brief load a shader from a file to a std::string
-   * @param shaderFile the std::filesystem::path to the file
-   * @return std::string the stringified version of the file
-   */
-  [[nodiscard]] VkFileResult
-      loadShaderDataFile(const std::filesystem::path &filePath);
+    /**
+     * @brief load a shader from a file to a std::string
+     * @param shaderFile the std::filesystem::path to the file
+     * @return std::string the stringified version of the file
+     */
+    [[nodiscard]] VkFileResult  loadShaderDataFile(const std::filesystem::path &filePath);
 
-  /**
-   * @brief Compile a GLSL Shader to Spriv
-   * @return a vector of compiled binary data representing the Spriv
-   * executable
-   */
-  const std::vector<uint32_t> compileGLSLToSPIRV(
-      const std::string &shaderCode,
-      const EShLanguage &shaderStage
-  );
+    /**
+     * @brief Compile a GLSL Shader to Spriv
+     * @return a vector of compiled binary data representing the Spriv
+     * executable
+     */
+    const std::vector<uint32_t> compileGLSLToSPIRV(const std::string &shaderCode, const EShLanguage &shaderStage);
 
-  std::string listAllShaders();
+    std::string                 listAllShaders();
 
-  ShaderDataFile *getShader(std::string name);
+    ShaderDataFile             *getShader(std::string name);
 
-  /**
-   * @brief loads all shaders to the given device
-   * @return the number of shader loaded to the given device
-   */
-  uint32_t loadAllShaders(const VkDevice &device);
+    [[nodiscard]] explicit      operator std::vector<VkShaderModuleCreateInfo>() const;
 
-  [[nodiscard]] explicit operator std::vector<VkShaderModuleCreateInfo>() const;
-
-  inline std::vector<VkShaderModuleCreateInfo> getAllShaderModulesCI() const
-  {
-    return static_cast<std::vector<VkShaderModuleCreateInfo>>(*this);
-    ;
-  }
+    inline std::vector<VkShaderModuleCreateInfo> getAllShaderModulesCI() const
+    {
+        return static_cast<std::vector<VkShaderModuleCreateInfo>>(*this);
+        ;
+    }
 };
 
-#endif // SHADER_HANDLER_HPP
+#endif    // SHADER_HANDLER_HPP

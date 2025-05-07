10505f9bb5ebb4025b6948792b0e56c5bcacd94e
Alex Boulay <alexandre.boulay59@gmail.com>
2025-05-07 21:29:58 +0200

    ShaderHandler Destruction Issue Fix
    Now need to work on individuals ShaderDataFiles.

--- ShaderHandler.cpp
+++ ShaderHandler.cpp
@@ -11,8 +11,7 @@
 #include <vulkan/vulkan_core.h>
 
 // @brief anonymous namespace to declare static elements
-namespace
-{
+namespace {
 
 // ---------------Static Types---------------------
 const std::map<std::string, SourcePlatform> extensionToPlatform = {
@@ -26,64 +25,70 @@ const std::map<std::string, SourcePlatform> extensionToPlatform = {
     {  ".py", SourcePlatform::Python}
 };
 
-struct ExtensionFileResult
-{
-    VkFileResult          status;
-    SourcePlatform        platform;
-    const stageExtention *stage;
+struct ExtensionFileResult {
+  VkFileResult          status;
+  SourcePlatform        platform;
+  const stageExtention *stage;
 };
 
 //----------------Static Functions-----------------
-[[nodiscard]] inline SourcePlatform getPlatformExt(std::string platExt)
+[[nodiscard]] inline SourcePlatform getPlatformExt(
+    std::string platExt
+)
 {
-    return extensionToPlatform.contains(platExt) ? extensionToPlatform.at(platExt) : SourcePlatform::Unknown;
+  return extensionToPlatform.contains(platExt) ? extensionToPlatform.at(platExt)
+                                               : SourcePlatform::Unknown;
 }
 
-[[nodiscard]] inline glslang::EShSource getEShSource(SourcePlatform sourcePlatform)
+[[nodiscard]] inline glslang::EShSource getEShSource(
+    SourcePlatform sourcePlatform
+)
 {
-    switch (sourcePlatform)
-    {
-    case (SourcePlatform::GLSL):
-        return glslang::EShSourceGlsl;
-    case (SourcePlatform::HLSL):
-        return glslang::EShSourceHlsl;
-    default:
-        return glslang::EShSourceNone;
-    }
+  switch (sourcePlatform) {
+    case (SourcePlatform::GLSL): return glslang::EShSourceGlsl;
+    case (SourcePlatform::HLSL): return glslang::EShSourceHlsl;
+    default: return glslang::EShSourceNone;
+  }
 }
 
-[[nodiscard]] ExtensionFileResult readExtentions(std::filesystem::path path)
+[[nodiscard]] ExtensionFileResult readExtentions(
+    std::filesystem::path path
+)
 {
-    std::vector<std::string> extensions;
-    auto                     filename = path.filename();
-    ExtensionFileResult result{.status = VK_FILE_EXTENTION_ERROR, .platform = SourcePlatform::Unknown, .stage = nullptr};
+  std::vector<std::string> extensions;
+  auto                     filename = path.filename();
+  ExtensionFileResult      result{
+           .status   = VK_FILE_EXTENTION_ERROR,
+           .platform = SourcePlatform::Unknown,
+           .stage    = nullptr
+  };
 
-    FILE_DEBUG_PRINT(" readExtensions - Filename : %s", filename.c_str());
-    while (filename.has_extension() && extensions.size() < 2)
-    {
-        FILE_DEBUG_PRINT("Extension : %s", filename.extension().c_str());
-        extensions.push_back(filename.extension().string());
-        filename = filename.stem();
+  FILE_DEBUG_PRINT(" readExtensions - Filename : %s", filename.c_str());
+  while (filename.has_extension() && extensions.size() < 2) {
+    FILE_DEBUG_PRINT("Extension : %s", filename.extension().c_str());
+    extensions.push_back(filename.extension().string());
+    filename = filename.stem();
+  }
+  if (!extensions.size() ||
+      !StageExtentionHandler::contains(extensions.back())) {
+    FILE_DEBUG_PRINT(
+        "File ignored due to unsuported extension : %s",
+        extensions.back().c_str()
+    );
+    return result; //.status = VK_FILE_EXTENSION ERROR
+  }
+  else {
+    result.stage = &(*StageExtentionHandler::at(extensions.back()));
+    extensions.pop_back();
+    if (extensions.size() && extensionToPlatform.contains(extensions.back())) {
+      result.platform = extensionToPlatform.at(extensions.back());
     }
-    if (!extensions.size() || !StageExtentionHandler::contains(extensions.back()))
-    {
-        FILE_DEBUG_PRINT("File ignored due to unsuported extension : %s", extensions.back().c_str());
-        return result;    //.status = VK_FILE_EXTENSION ERROR
-    }
-    else
-    {
-        result.stage = &(*StageExtentionHandler::at(extensions.back()));
-        extensions.pop_back();
-        if (extensions.size() && extensionToPlatform.contains(extensions.back()))
-        {
-            result.platform = extensionToPlatform.at(extensions.back());
-        }
-        FILE_DEBUG_PRINT("readExtensions Success");
-        result.status = VK_FILE_SUCCESS;
-    }
-    return result;
+    FILE_DEBUG_PRINT("readExtensions Success");
+    result.status = VK_FILE_SUCCESS;
+  }
+  return result;
 }
-};    // namespace
+}; // namespace
 
 //----------------ShaderDataFile::Functions---------------
 
@@ -138,134 +143,159 @@ VkResult ShaderDataFile::unload(
 */
 //--------------- ShaderHandler -----------------
 
-[[nodiscard]] VkFileResult ShaderHandler::loadShaderDataFile(const std::filesystem::path &filePath)
+[[nodiscard]] VkFileResult ShaderHandler::loadShaderDataFile(
+    const std::filesystem::path &filePath
+)
 {
-    if (std::filesystem::exists(filePath))
-    {
-        FILE_DEBUG_PRINT("Given path found : %s", filePath.string().c_str());
-        ExtensionFileResult extFileResult = readExtentions(filePath);
-        if (extFileResult.status == VK_FILE_SUCCESS)
-        {
-            FILE_DEBUG_PRINT("Extension(s) Loaded");
-            std::string shaderF = loadShaderFromFile(filePath);
-            FILE_DEBUG_PRINT("Shader Loaded");
-            std::vector<uint32_t> code = compileGLSLToSPIRV(shaderF, get<EShLanguage>(*extFileResult.stage));
-            FILE_DEBUG_PRINT("Compiled total number of uint32_t : %lu", code.size());
-            sDatas.push_back(ShaderDataFile(filePath.filename(), code, extFileResult.stage, extFileResult.platform));
-            FILE_DEBUG_PRINT("ShaderData added");
-            return VK_FILE_SUCCESS;
-        }
-        else
-        {
-            FILE_DEBUG_PRINT("Extension couldn't be loaded : %s ", filePath.c_str());
-            return VK_FILE_NOT_A_SHADER;
-        }
+  if (std::filesystem::exists(filePath)) {
+    FILE_DEBUG_PRINT("Given path found : %s", filePath.string().c_str());
+    ExtensionFileResult extFileResult = readExtentions(filePath);
+    if (extFileResult.status == VK_FILE_SUCCESS) {
+      FILE_DEBUG_PRINT("Extension(s) Loaded");
+      std::string shaderF = loadShaderFromFile(filePath);
+      FILE_DEBUG_PRINT("Shader Loaded");
+      std::vector<uint32_t> code =
+          compileGLSLToSPIRV(shaderF, get<EShLanguage>(*extFileResult.stage));
+      FILE_DEBUG_PRINT("Compiled total number of uint32_t : %lu", code.size());
+      sDatas.push_back(ShaderDataFile(
+          filePath.filename(),
+          code,
+          extFileResult.stage,
+          extFileResult.platform
+      ));
+      FILE_DEBUG_PRINT("ShaderData added");
+      return VK_FILE_SUCCESS;
     }
-    else
-    {
-        FILE_DEBUG_PRINT("At given path : %s - No shader elements found : ERROR ", filePath.string().c_str());
-        return VK_FILE_UNKNOWN_ERROR;
+    else {
+      FILE_DEBUG_PRINT("Extension couldn't be loaded : %s ", filePath.c_str());
+      return VK_FILE_NOT_A_SHADER;
     }
+  }
+  else {
+    FILE_DEBUG_PRINT(
+        "At given path : %s - No shader elements found : ERROR ",
+        filePath.string().c_str()
+    );
+    return VK_FILE_UNKNOWN_ERROR;
+  }
 }
 
-uint32_t ShaderHandler::loadShaderDataFromFolder(const std::filesystem::path &dirPath)
+uint32_t ShaderHandler::loadShaderDataFromFolder(
+    const std::filesystem::path &dirPath
+)
 {
-    uint32_t count = 0;
+  uint32_t count = 0;
 
-    if (!std::filesystem::is_directory(dirPath))
-    {
-        FILE_DEBUG_PRINT("The path is not a directory or does not exist.");
-    }
+  if (!std::filesystem::is_directory(dirPath)) {
+    FILE_DEBUG_PRINT("The path is not a directory or does not exist.");
+  }
 
-    for (auto f : std::filesystem::directory_iterator(dirPath))
-    {
-        std::cout << " count : " << count << " filename:  " << f.path().filename().string() << '\n';
-        count += std::filesystem::is_regular_file(f) && loadShaderDataFile(f) == VK_FILE_SUCCESS;
-    }
-    return const_cast<uint32_t &>(count);
+  for (auto f : std::filesystem::directory_iterator(dirPath)) {
+    std::cout << " count : " << count
+              << " filename:  " << f.path().filename().string() << '\n';
+    count += std::filesystem::is_regular_file(f) &&
+             loadShaderDataFile(f) == VK_FILE_SUCCESS;
+  }
+  return const_cast<uint32_t &>(count);
 }
 
-const std::string ShaderHandler::loadShaderFromFile(const std::filesystem::path &shaderFile)
+const std::string ShaderHandler::loadShaderFromFile(
+    const std::filesystem::path &shaderFile
+)
 {
-    std::ifstream file(shaderFile);
-    if (!file.is_open())
-    {
-        throw std::runtime_error(std::string("Couldn't load Shader File") + shaderFile.string());
-    }
-    return std::string((std::istreambuf_iterator<char>(file)), (std::istreambuf_iterator<char>()));
+  std::ifstream file(shaderFile);
+  if (!file.is_open()) {
+    throw std::runtime_error(
+        std::string("Couldn't load Shader File") + shaderFile.string()
+    );
+  }
+  return std::string(
+      (std::istreambuf_iterator<char>(file)),
+      (std::istreambuf_iterator<char>())
+  );
 }
 
-const std::vector<uint32_t> ShaderHandler::compileGLSLToSPIRV(const std::string &shaderCode,
-                                                              const EShLanguage &shaderStage)
+const std::vector<uint32_t> ShaderHandler::compileGLSLToSPIRV(
+    const std::string &shaderCode,
+    const EShLanguage &shaderStage
+)
 {
-    glslang::TShader                  shader(shaderStage);
-    const std::array<const char *, 1> shaderStrings = {shaderCode.c_str()};
+  glslang::TShader                  shader(shaderStage);
+  const std::array<const char *, 1> shaderStrings = {shaderCode.c_str()};
 
-    shader.setStrings(shaderStrings.data(), shaderStrings.size());
-    shader.setEnvInput(glslang::EShSourceGlsl, shaderStage, glslang::EShClientVulkan, OPENGL_CHOSEN_VERSION);
-    shader.setEnvClient(glslang::EShClientVulkan, VULKAN_CHOSEN_VERSION);
-    shader.setEnvTarget(glslang::EShTargetSpv, SPIRV_CHOSEN_VERSION);
+  shader.setStrings(shaderStrings.data(), shaderStrings.size());
+  shader.setEnvInput(
+      glslang::EShSourceGlsl,
+      shaderStage,
+      glslang::EShClientVulkan,
+      OPENGL_CHOSEN_VERSION
+  );
+  shader.setEnvClient(glslang::EShClientVulkan, VULKAN_CHOSEN_VERSION);
+  shader.setEnvTarget(glslang::EShTargetSpv, SPIRV_CHOSEN_VERSION);
 
-    if (!shader.parse(GetDefaultResources(), 100, false, EShMsgDefault))
-    {
-        FILE_DEBUG_PRINT("GLSL Parsing Failed for shader stage: %d ", shaderStage);
-        FILE_DEBUG_PRINT("%s\n%s", shader.getInfoLog(), shader.getInfoDebugLog());
-        return {};
-    }
+  if (!shader.parse(GetDefaultResources(), 100, false, EShMsgDefault)) {
+    FILE_DEBUG_PRINT("GLSL Parsing Failed for shader stage: %d ", shaderStage);
+    FILE_DEBUG_PRINT("%s\n%s", shader.getInfoLog(), shader.getInfoDebugLog());
+    return {};
+  }
 
-    glslang::TProgram program;
-    program.addShader(&shader);
+  glslang::TProgram program;
+  program.addShader(&shader);
 
-    if (!program.link(EShMsgDefault))
-    {
-        FILE_DEBUG_PRINT("GLSL Linking Failed : %s", program.getInfoLog());
-        FILE_DEBUG_PRINT("%s", program.getInfoDebugLog());
-        return {};
-    }
+  if (!program.link(EShMsgDefault)) {
+    FILE_DEBUG_PRINT("GLSL Linking Failed : %s", program.getInfoLog());
+    FILE_DEBUG_PRINT("%s", program.getInfoDebugLog());
+    return {};
+  }
 
-    std::vector<uint32_t>   spirv;
-    glslang::TIntermediate *intermediate = program.getIntermediate(shaderStage);
-    if (!intermediate)
-    {
-        FILE_DEBUG_PRINT("Failed to get intermediate representation");
-    }
+  std::vector<uint32_t>   spirv;
+  glslang::TIntermediate *intermediate = program.getIntermediate(shaderStage);
+  if (!intermediate) {
+    FILE_DEBUG_PRINT("Failed to get intermediate representation");
+  }
 
-    glslang::SpvOptions spvOptions;
-    spv::SpvBuildLogger logger;
-    glslang::GlslangToSpv(*intermediate, spirv, &logger, &spvOptions);
+  glslang::SpvOptions spvOptions;
+  spv::SpvBuildLogger logger;
+  glslang::GlslangToSpv(*intermediate, spirv, &logger, &spvOptions);
 
-    if (logger.getAllMessages().size())
-    {
-        FILE_DEBUG_PRINT("Spirv Logger : %s", logger.getAllMessages().c_str());
-    }
+  if (logger.getAllMessages().size()) {
+    FILE_DEBUG_PRINT("Spirv Logger : %s", logger.getAllMessages().c_str());
+  }
 
-    return spirv;
+  return spirv;
 }
 
-std::string ShaderHandler::listAllShaders()
+std::string ShaderHandler::listAllShaders() const
 {
-    return std::accumulate(sDatas.begin(),
-                           sDatas.end(),
-                           std::string(),
-                           [](const std::string &a, const ShaderDataFile &b) { return a + b.getName() + '\n'; });
+  return std::accumulate(
+      sDatas.begin(),
+      sDatas.end(),
+      std::string(),
+      [](const std::string &a, const ShaderDataFile &b) {
+        return a + b.getName() + '\n';
+      }
+  );
 }
 
-ShaderDataFile *ShaderHandler::getShader(std::string name)
+const ShaderDataFile *ShaderHandler::getShader(
+    std::string name
+) const
 {
-    std::vector<ShaderDataFile>::iterator result = std::find_if(sDatas.begin(),
-                                                                sDatas.end(),
-                                                                [&name](const ShaderDataFile &a) -> bool
-                                                                { return a.getName() == name; });
-    return result != sDatas.end() ? &(*result) : nullptr;
+  std::vector<ShaderDataFile>::const_iterator result = std::find_if(
+      sDatas.cbegin(),
+      sDatas.cend(),
+      [&name](const ShaderDataFile &a) -> bool { return a.getName() == name; }
+  );
+  return result != sDatas.end() ? &(*result) : nullptr;
 }
 
-[[nodiscard]] ShaderHandler::operator std::vector<VkShaderModuleCreateInfo>() const
+[[nodiscard]] ShaderHandler::operator std::vector<VkShaderModuleCreateInfo>(
+) const
 {
-    std::vector<VkShaderModuleCreateInfo> result;
-    result.reserve(sDatas.size());
-    for (const auto &a : sDatas)
-    {
-        result.push_back(static_cast<VkShaderModuleCreateInfo>(a));
-    }
-    return result;
+  std::vector<VkShaderModuleCreateInfo> result;
+  result.reserve(sDatas.size());
+  for (const auto &a : sDatas) {
+    result.push_back(static_cast<VkShaderModuleCreateInfo>(a));
+  }
+  return result;
 }

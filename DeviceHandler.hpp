631ac6b28df38fe695457487c90b284c91505951
Alexandre Boulay <alexandre.boulay59@gmail.com>
2025-05-09 14:43:05 +0200

    Moved Ressources Management
    Need to do imutable objects -> no move and direct management of
    ressources -> Ressource Wrapper that keeps pointers to allocations
    through Manager or whatever and can self destruct.

--- DeviceHandler.hpp
+++ DeviceHandler.hpp
@@ -12,164 +12,170 @@
 #include <vector>
 #include <vulkan/vulkan_core.h>
 
-namespace ABox_Utils {
+namespace ABox_Utils
+{
 
 /**
  * @struct QueueFamilyIndices
  * @brief represent optional queue family indices
  */
-struct QueueFamilyIndices {
-  std::optional<uint32_t> graphicQueueIndex;
-  std::optional<uint32_t> presentQueueIndex;
+struct QueueFamilyIndices
+{
+    std::optional<uint32_t> graphicQueueIndex;
+    std::optional<uint32_t> presentQueueIndex;
 };
 
 /**
  * @struct DeviceBoundElements
  * @brief Represents elements bounded to the Logical Device
  */
-struct DeviceBoundElements {
-  VkPhysicalDevice                                physical;
-  QueueFamilyIndices                              fIndices;
-  std::optional<SwapchainManager>                 swapchain;
-  std::optional<GraphicsPipeline>                 graphicsppl;
-  std::unordered_map<std::string, VkShaderModule> loadedShaders;
+struct DeviceBoundElements
+{
+    VkPhysicalDevice                                physical;
+    QueueFamilyIndices                              fIndices;
+    std::optional<SwapchainManager>                 swapchain;
+    std::optional<GraphicsPipeline>                 graphicsppl;
+    std::unordered_map<std::string, VkShaderModule> loadedShaders;
 };
 
 /**
  * @class DeviceHandler
  * @brief Handle specifics to logical devices and their different bindings
  */
-class DeviceHandler {
-  std::vector<VkPhysicalDevice>                          phyDevices;
-  std::vector<VkDevice>                                  devices;
-  std::unordered_map<uint_fast16_t, DeviceBoundElements> deviceMap;
+class DeviceHandler
+{
+    std::vector<VkPhysicalDevice>                          phyDevices;
+    std::vector<VkDevice>                                  devices;
+    std::unordered_map<uint_fast16_t, DeviceBoundElements> deviceMap;
 
-  std::set<uint32_t>    getQueueFamilyIndices(QueueFamilyIndices fi);
-  std::vector<uint32_t> listQueueFamilyIndices(QueueFamilyIndices fi);
+    std::set<uint32_t>                                     getQueueFamilyIndices(QueueFamilyIndices fi);
+    std::vector<uint32_t>                                  listQueueFamilyIndices(QueueFamilyIndices fi);
 
-   public:
-  DeviceHandler(
-      DeviceHandler &&other
-  ) noexcept
-      : phyDevices(other.phyDevices)
-      , devices(other.devices)
-  {
-    for (auto &a : other.deviceMap) {
-      deviceMap[a.first] = {
-          .physical      = a.second.physical,
-          .fIndices      = a.second.fIndices,
-          .swapchain     = std::move(a.second.swapchain),
-          .graphicsppl   = std::move(a.second.graphicsppl),
-          .loadedShaders = std::move(a.second.loadedShaders
-          ) // Wrap ShaderModules into own class ?
-      };
-      a.second.graphicsppl.reset();
-      a.second.swapchain.reset();
+public:
+    DeviceHandler(DeviceHandler &&other) noexcept
+        : phyDevices(other.phyDevices)
+        , devices(other.devices)
+    {
+        for (auto &a : other.deviceMap)
+        {
+            deviceMap[a.first] = {
+                .physical      = a.second.physical,
+                .fIndices      = a.second.fIndices,
+                .swapchain     = std::move(a.second.swapchain),
+                .graphicsppl   = std::move(a.second.graphicsppl),
+                .loadedShaders = std::move(a.second.loadedShaders)    // Wrap ShaderModules into own class ?
+            };
+            a.second.graphicsppl.reset();
+            a.second.swapchain.reset();
+        }
     }
-  }
 
-  ~DeviceHandler()
-  {
-    for (auto &a : deviceMap) {
-      for (auto &pair : a.second.loadedShaders) {
-        vkDestroyShaderModule(devices[a.first], pair.second, nullptr);
-      }
-      a.second.loadedShaders.clear();
+    ~DeviceHandler()
+    {
+        std::cout << " Destruction : Device Map : Size " << deviceMap.size() << std::endl;
+        for (auto &a : deviceMap)
+        {
+            std::cout << "Device : " << a.first << " - Shaders " << a.second.loadedShaders.size() << std::endl;
+            for (auto &pair : a.second.loadedShaders)
+            {
+                std::cout << "Shader name " << pair.first << std::endl;
+                vkDestroyShaderModule(devices[a.first], pair.second, nullptr);
+            }
+            // a.second.loadedShaders.clear();
+        }
+        std::cout << "Clearing device map" << std::endl;
+        for (auto &a : deviceMap)
+        {
+            if (a.second.swapchain.has_value())
+            {
+                std::cout << "Removing swapchainandler from device." << std::endl;
+                a.second.swapchain.reset();
+            }
+        }
+        // deviceMap.clear();
+        for (auto &dev : devices)
+        {
+            if (dev != VK_NULL_HANDLE)
+            {
+                std::cout << "Destroying device :" << (void *) dev << std::endl;
+                vkDestroyDevice(dev, nullptr);
+                std::cout << "Device destroyed : " << (dev == VK_NULL_HANDLE);
+            }
+        }
+        // devices.clear();
     }
-  }
 
-  DeviceHandler &operator=(
-      DeviceHandler &&other
-  ) noexcept
-  {
-    if (this != &other) {
-      // Copy assignment: copy the members from `other` to `this`
-      phyDevices = other.phyDevices;
-      devices    = other.devices;
+    DeviceHandler &operator=(DeviceHandler &&other) noexcept
+    {
+        if (this != &other)
+        {
+            // Copy assignment: copy the members from `other` to `this`
+            phyDevices = other.phyDevices;
+            devices    = other.devices;
 
-      other.phyDevices.clear();
-      other.devices.clear();
-      for (auto &a : other.deviceMap) {
-        deviceMap[a.first] = {
-            .physical      = a.second.physical,
-            .fIndices      = a.second.fIndices,
-            .swapchain     = std::move(a.second.swapchain),
-            .graphicsppl   = std::move(a.second.graphicsppl),
-            .loadedShaders = std::move(a.second.loadedShaders)
-        };
-        a.second.graphicsppl.reset();
-        a.second.swapchain.reset();
-      }
+            other.phyDevices.clear();
+            other.devices.clear();
+            for (auto &a : other.deviceMap)
+            {
+                deviceMap[a.first] = {.physical      = a.second.physical,
+                                      .fIndices      = a.second.fIndices,
+                                      .swapchain     = std::move(a.second.swapchain),
+                                      .graphicsppl   = std::move(a.second.graphicsppl),
+                                      .loadedShaders = std::move(a.second.loadedShaders)};
+                a.second.graphicsppl.reset();
+                a.second.swapchain.reset();
+            }
+        }
+        return *this;
     }
-    return *this;
-  }
 
-  VkResult listPhysicalDevices() const;
+    VkResult           listPhysicalDevices() const;
 
-  /**
-   * @brief add a Logical device while guessing which Physical Device is the
-   * best suited to do the job
-   *
-   * @param VkSurfaceKHR surface the surface which will bind presentation
-   * @return VkResult : VK_SUCCESS if succeded else a corresponding error
-   * value
-   */
-  VkResult addLogicalDevice(VkSurfaceKHR surface);
-  /**
-   *
-   * @param uint32_t index the physical device index to select
-   * @param[[VkSurfaceKHR] surface the surface which will bind presentation
-   * @return a VkResult if VK_SUCCESS if succeded else a corresponding error
-   * value
-   */
-  VkResult addLogicalDevice(uint32_t index, VkSurfaceKHR surface);
+    /**
+     * @brief add a Logical device while guessing which Physical Device is the
+     * best suited to do the job
+     *
+     * @param VkSurfaceKHR surface the surface which will bind presentation
+     * @return VkResult : VK_SUCCESS if succeded else a corresponding error
+     * value
+     */
+    VkResult           addLogicalDevice(VkSurfaceKHR surface);
+    /**
+     *
+     * @param uint32_t index the physical device index to select
+     * @param[[VkSurfaceKHR] surface the surface which will bind presentation
+     * @return a VkResult if VK_SUCCESS if succeded else a corresponding error
+     * value
+     */
+    VkResult           addLogicalDevice(uint32_t index, VkSurfaceKHR surface);
 
-  // TODO : VkResult clear();
-  uint32_t listQueueFamilies();
-  VkResult DeviceExtensionSupport(VkPhysicalDevice device);
-  QueueFamilyIndices
-      loadNecessaryQueueFamilies(uint32_t phyDev, VkSurfaceKHR surface);
+    // TODO : VkResult clear();
+    uint32_t           listQueueFamilies();
+    VkResult           DeviceExtensionSupport(VkPhysicalDevice device);
+    QueueFamilyIndices loadNecessaryQueueFamilies(uint32_t phyDev, VkSurfaceKHR surface);
 
-  uint32_t findBestPhysicalDevice();
+    uint32_t           findBestPhysicalDevice();
 
-  DeviceHandler() {};
-  DeviceHandler(VkInstance instance);
+    DeviceHandler() {};
+    DeviceHandler(VkInstance instance);
 
-  void removeBindings();
+    VkDevice   *getDevice(uint32_t index);
 
-  VkDevice *getDevice(uint32_t index);
+    inline bool hasDevice(uint32_t index) const { return index < devices.size(); }
 
-  inline bool hasDevice(
-      uint32_t index
-  ) const
-  {
-    return index < devices.size();
-  }
+    // DeviceBoundElements getBoundElements(uint_fast16_t devIndex) const;
+    VkResult    addSwapchain(uint32_t width, uint32_t height, VkSurfaceKHR *surface, uint_fast8_t devIndex);
 
-  // DeviceBoundElements getBoundElements(uint_fast16_t devIndex) const;
-  VkResult addSwapchain(
-      uint32_t      width,
-      uint32_t      height,
-      VkSurfaceKHR *surface,
-      uint_fast8_t  devIndex
-  );
+    std::pair<VkResult, VkShaderModule> loadShader(uint_fast16_t deviceIndex, const ShaderDataFile &sdf);
 
-  std::pair<VkResult, VkShaderModule>
-      loadShader(uint_fast16_t deviceIndex, const ShaderDataFile &sdf);
+    /**
+     * @brief add a GraphicsPipeline to a LogicalDevice which must have a
+     * swapchain
+     */
+    VkResult addGraphicsPipeline(uint32_t deviceIndex, const std::list<ShaderDataFile> &shaderFiles);
 
-  /**
-   * @brief add a GraphicsPipeline to a LogicalDevice which must have a
-   * swapchain
-   */
-  VkResult addGraphicsPipeline(
-      uint32_t                         deviceIndex,
-      const std::list<ShaderDataFile> &shaderFiles
-  );
-
-  // No copy
-  DELETE_COPY(
-      DeviceHandler
-  )
+    // No copy
+    DELETE_COPY(DeviceHandler)
 };
 
 std::stringstream vkQueueFlagSS(const VkQueueFlags &flag);
@@ -177,5 +183,5 @@ OSTREAM_OP(const VkQueueFamilyProperties &prop);
 OSTREAM_OP(const VkExtent3D &ext);
 OSTREAM_OP(const VkPhysicalDeviceProperties &phyP);
 OSTREAM_OP(const VkPhysicalDeviceType &phyT);
-} // namespace ABox_Utils
+}    // namespace ABox_Utils
 #endif

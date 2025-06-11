61114b8b81a812fffede32370518f2fb42f9a229
Alex Boulay <alexandre.boulay59@gmail.com>
2025-06-12 01:38:54 +0200

    DrawFame first part
    Need to separate each steps into appropriate place.

--- src/core/ResourcesManager.hpp
+++ src/core/ResourcesManager.hpp
@@ -140,7 +140,59 @@ class ResourcesManager {
   }
 
   void drawFrame()
-  { // TODO !!
+  {
+    // SYNCHRONISTATION --------------
+    vkWaitForFences(device, 1, &inFlightFence, VK_TRUE, UINT64_MAX);
+    vkResetFences(device, 1, &inFlightFence);
+
+    uint32_t imageIndex;
+    vkAcquireNextImageKHR(
+        device,
+        swapChain,
+        UINT64_MAX,
+        imageAvailableSemaphore,
+        VK_NULL_HANDLE,
+        &imageIndex
+    );
+
+    vkResetCommandBuffer(commandBuffer, /*VkCommandBufferResetFlagBits*/ 0);
+    recordCommandBuffer(commandBuffer, imageIndex);
+    // SUBMIT -----------
+    VkSemaphore waitSemaphores[]   = {imageAvailableSemaphore};
+    VkSemaphore signalSemaphores[] = {renderFinishedSemaphore};
+
+    VkPipelineStageFlags waitStages[] = {
+        VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT
+    };
+    VkSubmitInfo submitInfo{
+        .sType                = VK_STRUCTURE_TYPE_SUBMIT_INFO,
+        .pNext                = nullptr,
+        .waitSemaphoreCount   = 1u,
+        .pWaitSemaphores      = waitSemaphores,
+        .pWaitDstStageMask    = waitStages,
+        .commandBufferCount   = 1u,
+        .pCommandBuffers      = &commandBuffer,
+        .signalSemaphoreCount = 1,
+        .pSignalSemaphores    = signalSemaphores,
+    };
+    if (vkQueueSubmit(graphicsQueue, 1, &submitInfo, inFlightFence) !=
+        VK_SUCCESS) {
+      throw std::runtime_error("failed to submit draw command buffer!");
+    }
+
+    // PRESENT
+    VkSwapchainKHR   swapChains[] = {swapChain};
+    VkPresentInfoKHR presentInfo{
+        .sType              = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR,
+        .pNext              = nullptr,
+        .waitSemaphoreCount = 1u,
+        .pWaitSemaphores    = signalSemaphores,
+        .swapchainCount     = 1u,
+        .pSwapchains        = swapChains,
+        .pImageIndices      = &imageIndex,
+        .pResults           = nullptr
+    };
+    vkQueuePresentKHR(presentQueue, &presentInfo);
     return;
   }
 };
